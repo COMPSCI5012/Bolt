25b620ca91830ce8f64df0286dd81b9cb4a7b543
heeking13 <57116370+heeking13@users.noreply.github.com>
2021-08-06 11:21:17 +0100

    update admin.py to add fqa models

--- zootopia/bolt/admin.py
+++ zootopia/bolt/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from bolt.models import Animal, Shelter, UserProfile
+from bolt.models import Animal, Shelter, UserProfile, Fqa
 
 class ShelterAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
@@ -8,3 +8,4 @@ class ShelterAdmin(admin.ModelAdmin):
 admin.site.register(Animal)
 admin.site.register(Shelter, ShelterAdmin)
 admin.site.register(UserProfile)
+admin.site.register(Fqa)
